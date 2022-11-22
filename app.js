c43d0e37e5057d269bd92292b8776f2c18abc269
mirokolehmainen <miro.kolehmainen@student.laurea.fi>
2022-11-22 21:33:57 +0200

    muutoksia

--- app.js
+++ app.js
@@ -1,282 +1,291 @@
-//Tekee uuden xml pyynnön nappia painaessa
+//Tekee uuden xml pyynnön nappia painaessa ja kokeillaan, että kaikki toimii
 document.getElementById('1').onclick = function Omena() {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            myFunction(this);
+            Hakija(this);
         }
     };
     //Hakee tiedot
     xmlhttp.open(
-        'GET',
-        'https://www.finnkino.fi/xml/Schedule/?area=1039',
-        true
+        'GET','https://www.finnkino.fi/xml/Schedule/?area=1039', true
     );
     //Lähettää tiedot
     xmlhttp.send();
 }
 
+//Tekee uuden xml pyynnön nappia painaessa ja kokeillaan, että kaikki toimii
 document.getElementById('2').onclick= function Sello() {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            myFunction(this);
+            Hakija(this);
         }
     };
-
+    //Hakee tiedot
     xmlhttp.open(
-        'GET',
-        'https://www.finnkino.fi/xml/Schedule/?area=1038',
-        true
+        'GET','https://www.finnkino.fi/xml/Schedule/?area=1038', true
     );
+    //Lähettää tiedot
     xmlhttp.send();
 }
 
+//Tekee uuden xml pyynnön nappia painaessa ja kokeillaan, että kaikki toimii
 document.getElementById('3').onclick = function Itis() {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            myFunction(this);
+            Hakija(this);
         }
     };
 
+    //Hakee tiedot
     xmlhttp.open(
-        'GET',
-        'https://www.finnkino.fi/xml/Schedule/?area=1045',
-        true
+        'GET','https://www.finnkino.fi/xml/Schedule/?area=1045', true
     );
+    //Lähettää tiedot
     xmlhttp.send();
 }
 
+//Tekee uuden xml pyynnön nappia painaessa ja kokeillaan, että kaikki toimii
 document.getElementById('4').onclick =function Kinopalatsi() {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            myFunction(this);
+            Hakija(this);
         }
     };
 
+    //Hakee tiedot
     xmlhttp.open(
-        'GET',
-        'https://www.finnkino.fi/xml/Schedule/?area=1031',
-        true
+        'GET','https://www.finnkino.fi/xml/Schedule/?area=1031', true
     );
+    //Lähettää tiedot
     xmlhttp.send();
 }
 
+//Tekee uuden xml pyynnön nappia painaessa ja kokeillaan, että kaikki toimii
 document.getElementById('5').onclick = function Maxim() {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            myFunction(this);
+            Hakija(this);
         }
     };
 
+    //Hakee tiedot
     xmlhttp.open(
-        'GET',
-        'https://www.finnkino.fi/xml/Schedule/?area=1032',
-        true
+        'GET', 'https://www.finnkino.fi/xml/Schedule/?area=1032', true
     );
+    //Lähettää tiedot
     xmlhttp.send();
 }
 
+//Tekee uuden xml pyynnön nappia painaessa ja kokeillaan, että kaikki toimii
 document.getElementById('6').onclick = function Tennispalatsi() {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            myFunction(this);
+            Hakija(this);
         }
     };
 
+    //Hakee tiedot
     xmlhttp.open(
-        'GET',
-        'https://www.finnkino.fi/xml/Schedule/?area=1033',
-        true
+        'GET','https://www.finnkino.fi/xml/Schedule/?area=1033', true
     );
+    //Lähettää tiedot
     xmlhttp.send();
 }
 
-
+//Tekee uuden xml pyynnön nappia painaessa ja kokeillaan, että kaikki toimii
 document.getElementById('7').onclick = function Flamingo() {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            myFunction(this);
+            Hakija(this);
         }
     };
 
+    //Hakee tiedot
     xmlhttp.open(
-        'GET',
-        'https://www.finnkino.fi/xml/Schedule/?area=1013',
-        true
+        'GET','https://www.finnkino.fi/xml/Schedule/?area=1013', true
     );
+    //Lähettää tiedot
     xmlhttp.send();
 }
 
+//Tekee uuden xml pyynnön nappia painaessa ja kokeillaan, että kaikki toimii
 document.getElementById('8').onclick = function Fantasia() {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            myFunction(this);
+            Hakija(this);
         }
     };
 
+    //Hakee tiedot
     xmlhttp.open(
-        'GET',
-        'https://www.finnkino.fi/xml/Schedule/?area=1015',
-        true
+        'GET', 'https://www.finnkino.fi/xml/Schedule/?area=1015', true
     );
+    //Lähettää tiedot
     xmlhttp.send();
 }
+
+//Tekee uuden xml pyynnön nappia painaessa ja kokeillaan, että kaikki toimii
 document.getElementById('9').onclick = function Scala() {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            myFunction(this);
+            Hakija(this);
         }
     };
 
+    //Hakee tiedot
     xmlhttp.open(
-        'GET',
-        'https://www.finnkino.fi/xml/Schedule/?area=1016',
-        true
+        'GET', 'https://www.finnkino.fi/xml/Schedule/?area=1016', true
     );
+    //Lähettää tiedot
     xmlhttp.send();
 }
 
+//Tekee uuden xml pyynnön nappia painaessa ja kokeillaan, että kaikki toimii
 document.getElementById('10').onclick = function Kuvapalatsi() {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            myFunction(this);
+            Hakija(this);
         }
     };
 
+    //Hakee tiedot
     xmlhttp.open(
-        'GET',
-        'https://www.finnkino.fi/xml/Schedule/?area=1017',
-        true
+        'GET', 'https://www.finnkino.fi/xml/Schedule/?area=1017', true
     );
+    //Lähettää tiedot
     xmlhttp.send();
 }
 
+//Tekee uuden xml pyynnön nappia painaessa ja kokeillaan, että kaikki toimii
 document.getElementById('11').onclick = function Strand() {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            myFunction(this);
+            Hakija(this);
         }
     };
 
+    //Hakee tiedot
     xmlhttp.open(
-        'GET',
-        'https://www.finnkino.fi/xml/Schedule/?area=1041',
-        true
+        'GET','https://www.finnkino.fi/xml/Schedule/?area=1041', true
     );
+    //Lähettää tiedot
     xmlhttp.send();
 }
 
+//Tekee uuden xml pyynnön nappia painaessa ja kokeillaan, että kaikki toimii
 document.getElementById('12').onclick = function Plaza() {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            myFunction(this);
+            Hakija(this);
         }
     };
 
+    //Hakee tiedot
     xmlhttp.open(
-        'GET',
-        'https://www.finnkino.fi/xml/Schedule/?area=1018',
-        true
+        'GET','https://www.finnkino.fi/xml/Schedule/?area=1018', true
     );
+    //Lähettää tiedot
     xmlhttp.send();
 }
 
-
+//Tekee uuden xml pyynnön nappia painaessa ja kokeillaan, että kaikki toimii
 document.getElementById('13').onclick = function Promenadi() {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            myFunction(this);
+            Hakija(this);
         }
     };
 
+    //Hakee tiedot
     xmlhttp.open(
-        'GET',
-        'https://www.finnkino.fi/xml/Schedule/?area=1019',
-        true
+        'GET','https://www.finnkino.fi/xml/Schedule/?area=1019', true
     );
+    //Lähettää tiedot
     xmlhttp.send();
 }
 
-
+//Tekee uuden xml pyynnön nappia painaessa ja kokeillaan, että kaikki toimii
 document.getElementById('14').onclick = function Atlas() {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            myFunction(this);
+            Hakija(this);
         }
     };
 
+    //Hakee tiedot
     xmlhttp.open(
-        'GET',
-        'https://www.finnkino.fi/xml/Schedule/?area=1034',
-        true
+        'GET','https://www.finnkino.fi/xml/Schedule/?area=1034', true
     );
+    //Lähettää tiedot
     xmlhttp.send();
 }
 
-
-
+//Tekee uuden xml pyynnön nappia painaessa ja kokeillaan, että kaikki toimii
 document.getElementById('15').onclick = function Plevna() {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            myFunction(this);
+            Hakija(this);
         }
     };
 
+    //Hakee tiedot
     xmlhttp.open(
-        'GET',
-        'https://www.finnkino.fi/xml/Schedule/?area=1035',
-        true
+        'GET', 'https://www.finnkino.fi/xml/Schedule/?area=1035', true
     );
+    //Lähettää tiedot
     xmlhttp.send();
 }
 
+//Tekee uuden xml pyynnön nappia painaessa ja kokeillaan, että kaikki toimii
 document.getElementById('16').onclick = function TurkuKino() {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            myFunction(this);
+            Hakija(this);
         }
     };
 
+    //Hakee tiedot
     xmlhttp.open(
-        'GET',
-        'https://www.finnkino.fi/xml/Schedule/?area=1022',
-        true
+        'GET', 'https://www.finnkino.fi/xml/Schedule/?area=1022', true
     );
+    //Lähettää tiedot
     xmlhttp.send();
 }
 
+//Tekee uuden xml pyynnön nappia painaessa ja kokeillaan, että kaikki toimii
 document.getElementById('17').onclick = function LuxeMylly() {
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            myFunction(this);
+            Hakija(this);
         }
     };
 
+    //Hakee tiedot
     xmlhttp.open(
-        'GET',
-        'https://www.finnkino.fi/xml/Schedule/?area=1046',
-        true
+        'GET', 'https://www.finnkino.fi/xml/Schedule/?area=1046', true
     );
+    //Lähettää tiedot
     xmlhttp.send();
 }
 //Tehdään muuttujia ja taulukko xml tiedoille
-function myFunction(xml) {
+function Hakija(xml) {
     var i;
     var xmlDoc = xml.responseXML;
     var table =
@@ -286,36 +295,25 @@ function myFunction(xml) {
     var x = xmlDoc.getElementsByTagName('Show');
     for (i = 0; i < x.length; i++) {
 
-        //Haetaan kuvia ja lisätään ne muihin hakuihin
-        const Kuva = x[i].getElementsByTagName('Images')[0]
-            .firstElementChild.textContent;
+        //Haetaan kuvia ja otetaan ne huomioon muissa hauissa
+        const Kuva = x[i].getElementsByTagName('Images')[0].firstElementChild.textContent;
         table +=
-        //Lisätään tiedot taulukkoon
+        //Lisätään tiedot taulukkoon hakemalla arvoja
             '<tr><td>' +
-            x[i].getElementsByTagName('Genres')[0].childNodes[0]
-                .nodeValue +
+            x[i].getElementsByTagName('Genres')[0].childNodes[0].nodeValue +
             '</td><td>' +
-            x[i].getElementsByTagName('Title')[0].childNodes[0]
-                .nodeValue +
-                '</td><td>' +
-             x[i].getElementsByTagName('Rating')[0].childNodes[0]
-                .nodeValue +
-                '</td><td>' +
-            x[i].getElementsByTagName('PresentationMethod')[0].childNodes[0]
-            .nodeValue + 
+            x[i].getElementsByTagName('Title')[0].childNodes[0].nodeValue +
             '</td><td>' +
-            x[i].getElementsByTagName('dttmShowStart')[0].childNodes[0]
-            .nodeValue + 
+            x[i].getElementsByTagName('Rating')[0].childNodes[0].nodeValue +
             '</td><td>' +
-            x[i].getElementsByTagName('TheatreAuditorium')[0].childNodes[0]
-                .nodeValue + 
+            x[i].getElementsByTagName('PresentationMethod')[0].childNodes[0].nodeValue + 
             '</td><td>' +
-            x[i].getElementsByTagName('ProductionYear')[0].childNodes[0]
-                .nodeValue + 
-                '</td>' +
-            '<td><img src=' +
-            Kuva +
-            '></td></tr>';
+            x[i].getElementsByTagName('dttmShowStart')[0].childNodes[0].nodeValue + 
+            '</td><td>' +
+            x[i].getElementsByTagName('TheatreAuditorium')[0].childNodes[0].nodeValue + 
+            '</td><td>' +
+            x[i].getElementsByTagName('ProductionYear')[0].childNodes[0].nodeValue + 
+            '</td>' + '<td><img src=' + Kuva + '></td></tr>';
     }
     //Haetaan Talukko ja yhdistetään se tehtyyn taulokkoon
     document.getElementById('Taulukko').innerHTML = table;
